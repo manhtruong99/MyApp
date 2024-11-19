b36dac30fde347b1865f4fbd911a05abe588165a
manhtruong99 <puddingvux@gmail.com>
2024-11-19 17:07:17 +0700

    test

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,6 +1,6 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -12,12 +12,9 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  server: {
-    port: 2511
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
